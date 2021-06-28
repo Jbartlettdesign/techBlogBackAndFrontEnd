ec3abe9c8c63d7040cbc11c9c28a95a6d21ce925
John <jbartlettdesign@gmail.com>
2021-06-27 20:13:03 -0500

    setup with routes/models/css

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const{User, Post, Comment} = require('../../models');
+
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes:[
+            'id',
+            'user_id',
+            'post_id',
+            'comment_text'
+        ],
+    }).then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+router.post('/', (req, res) => {
+    Comment.create({
+        user_id:req.body.user_id,
+        post_id:req.body.post_id,
+        comment_text:req.body.comment_text
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+        //400 Bad Request response status code indicates that the server cannot or will not process the request due to something that is perceived to be a client error
+    })
+})
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+/////allow us to use public folder/css/js
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+////////////////////////////////////
+///////////////////////////////////////
+//connetcion to sequelize
+
+//////////////////////////////////////
+
+/////////////////////////////////////
+/*const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Mysecret',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db:sequelize
+    })
+};
+app.use(session(sess));*/
+/////////////////////////////////
+app.use(routes);
+////////////////////////////////
+//handlebars
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+///////////////////////////////////////////
+//turn on connection to db
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log('Now Listening'));
+})
